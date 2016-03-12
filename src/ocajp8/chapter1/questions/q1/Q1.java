0d9e2f53f49b6982842ed10ed5a106435edb6595
Dhiego Abrantes <dhiegoabrantes@gmail.com>
2016-03-11 23:38:25 -0300

    commit inicial ocajp8 :)

---
+++ src/ocajp8/chapter1/questions/q1/Q1.java
@@ -0,0 +1,19 @@
+package ocajp8.chapter1.questions.q1;
+
+public class Q1 {
+
+	//Which of the following are valid Java identifiers? (Choose all that apply) 
+	//	A. A$B
+	//	B. _helloWorld
+	//	C. true
+	//	D. java.lang
+	//	E. Public
+	//	F. 1980_s
+	
+	//	int A$B; (correct)
+	//	int _helloWorld; (correct)
+	//	int true;
+	//	int java.lang;
+	//	int Public; (correct, P está maiúsculo! haha! :D )
+	//	int 1980_s;
+}
